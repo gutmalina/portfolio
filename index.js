db94d489ff5d5b49bc4b97f166e899eac62a8d18
Marina Gutkevich <gutmalina@mail.ru>
2023-04-10 11:45:25 +0300

    make visual

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const letterList = document.querySelectorAll('.greeting__letter');
 
+letterList.forEach((letter, item)=>{
+  const count = item/9
+  letter.style.setProperty('animation', `loadingLetter 0.5s linear ${count}s forwards`)
+})
