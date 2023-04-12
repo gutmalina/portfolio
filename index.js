e021484b556cf6ab0c3ca4c72ee9b8c794364aba
Marina Gutkevich <gutmalina@mail.ru>
2023-04-12 19:14:06 +0300

    make adaptive layout

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const letterList = document.querySelectorAll('.greeting__letter');
+const menuBtn = document.querySelector('.menu');
+const menuBtnClose = document.querySelector('.header__close');
+const header = document.querySelector('.header');
 
 letterList.forEach((letter, item)=>{
   const count = item/9
   letter.style.setProperty('animation', `loadingLetter 0.5s linear ${count}s forwards`)
-})
+});
+
+const openHeaderMenu = () => {
+  header.style.setProperty('display', 'flex');
+};
+
+const closeHeaderMenu = () =>{
+  header.style.removeProperty('display');
+};
+
+menuBtn.addEventListener('click', openHeaderMenu);
+menuBtnClose.addEventListener('click', closeHeaderMenu);
